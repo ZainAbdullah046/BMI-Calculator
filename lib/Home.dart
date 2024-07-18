c0269645b2a5fd9bf2ca070bde69dcf11ed42285
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-07-19 00:20:58 +0500

    bmi calculator

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,19 +8,41 @@ class Home extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<Home> {
+  final TextEditingController wicontroller = TextEditingController();
+  final TextEditingController hecontroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: const Center(
-        child: Text(
-          "BMI CALCULATOR",
-          style: TextStyle(
-              color: Color.fromARGB(179, 19, 5, 5),
-              fontWeight: FontWeight.bold,
-              fontSize: 20),
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            "BMI CALCULATOR",
+            style: TextStyle(
+                color: Color.fromARGB(179, 19, 5, 5),
+                fontWeight: FontWeight.bold,
+                fontSize: 20),
+          ),
         ),
       ),
-    ));
+      body: Column(
+        children: [
+          TextField(
+            controller: wicontroller,
+            decoration: const InputDecoration(
+                hintText: "Enter your weight in kgs",
+                prefixIcon: Icon(Icons.line_weight)),
+            keyboardType: TextInputType.number,
+          ),
+          TextField(
+            controller: hecontroller,
+            decoration: const InputDecoration(
+                hintText: "Enter your height meters",
+                prefixIcon: Icon(Icons.line_weight)),
+            keyboardType: TextInputType.number,
+          )
+        ],
+      ),
+    );
   }
 }
